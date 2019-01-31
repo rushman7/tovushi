87b276dad5bbf71dbc3276d9883634ceb45b3af7
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-31 18:05:44 -0500

    Nav bar css done

--- tovushi/src/Components/Nav.js
+++ tovushi/src/Components/Nav.js
@@ -6,27 +6,38 @@ class Nav extends Component {
     return (
       <div className="nav">
         <Link
+          className="home-nav"
+          to="/"
+          style={{ textDecoration: "none"}}>
+          <i class="fa fa-home" aria-hidden="true"></i>
+          <span className="span">Home</span>
+        </Link>
+        <Link
           className="about-nav"
           to="/about"
           style={{ textDecoration: "none"}}>
+          <i class="fa fa-info-circle" aria-hidden="true"></i>
           <span className="span">About</span>
         </Link>
         <Link
           className="classes-nav"
           to="/classes"
           style={{ textDecoration: "none"}}>
+          <i class="fa fa-book" aria-hidden="true"></i>
           <span className="span">Classes</span>
         </Link>
         <Link
           className="schedule-nav"
           to="/schedule"
           style={{ textDecoration: "none"}}>
+          <i class="fa fa-calendar" aria-hidden="true"></i>
           <span className="span">Schedule</span>
         </Link>
         <Link
           className="contact-nav"
           to="/contact"
           style={{ textDecoration: "none"}}>
+          <i class="fa fa-phone" aria-hidden="true"></i>
           <span className="span">Contact</span>
         </Link>
       </div>

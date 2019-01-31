3390b1019cc383ece1377f957acb76efcbe946c4
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-31 17:00:41 -0500

    nav bar content

--- tovushi/src/Components/Nav.js
+++ tovushi/src/Components/Nav.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
   render() {
     return (
       <div className="nav">
-        <button>Nav Bar Button</button>
+        <Link
+          className="about-nav"
+          to="/about"
+          style={{ textDecoration: "none"}}>
+          <span className="span">About</span>
+        </Link>
+        <Link
+          className="classes-nav"
+          to="/classes"
+          style={{ textDecoration: "none"}}>
+          <span className="span">Classes</span>
+        </Link>
+        <Link
+          className="schedule-nav"
+          to="/schedule"
+          style={{ textDecoration: "none"}}>
+          <span className="span">Schedule</span>
+        </Link>
+        <Link
+          className="contact-nav"
+          to="/contact"
+          style={{ textDecoration: "none"}}>
+          <span className="span">Contact</span>
+        </Link>
       </div>
     )
   }

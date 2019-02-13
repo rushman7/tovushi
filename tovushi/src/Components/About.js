38d470f80120a6d6cfeb91d7a1e8e38eb6bfeeaf
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-13 18:06:19 -0500

    about page css

--- tovushi/src/Components/About.js
+++ tovushi/src/Components/About.js
@@ -16,31 +16,31 @@ class About extends Component {
             <p className="about-text-1">
               We are a kids community center located in Brooklyn, NY. We have children ranging from all ages 5 and up. 
             </p>
-            <img src={FrontSide} alt="FrontSide" />;
+            <img className="images" src={FrontSide} alt="FrontSide" />
           </div>
           <div className="about-content">
             <p className="about-text-2">
               A dance team, a soccer team, chess, drums and many more. 
             </p>
-            <img src={Drums} alt="Drums" />;
+            <img className="images" src={Drums} alt="Drums" />
           </div>
           <div className="about-content">
             <p className="about-text-3">
               Our dance ensemble are winners of multiple national dance competitions and our soccer team has participated in many soccer championships and coming as close as second place nation-wide. 
             </p>
-            <img src={Soccer} alt="Soccer" />;
+            <img className="images" src={Soccer} alt="Soccer" />
           </div>
           <div className="about-content">
             <p className="about-text-4">
               During the summer we also have a daily camp where children go on many trips including water parks, movie theatres, museums, etc. 
             </p>
-            <img src={PurimParty} alt="PurimParty" />;
+            <img className="images" src={PurimParty} alt="PurimParty" />
           </div>
           <div className="about-content">
             <p className="about-text-5">
               We would love to continue bringing kids together where it is to our dance classes, soccer classes, or our family friendly day camp.
             </p>
-            <img src={DanceGroup} alt="DanceGroup" />;
+            <img className="images" src={DanceGroup} alt="DanceGroup" />
           </div>
         </div>
       </React.Fragment>

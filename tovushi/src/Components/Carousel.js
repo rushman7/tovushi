c260c09b1c9ad84fb1bc5ec722a3f9a28e0d13f3
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-14 17:25:01 -0500

    updated css, removing reactstrap carousel

--- tovushi/src/Components/Carousel.js
+++ tovushi/src/Components/Carousel.js
@@ -1,89 +1,36 @@
 import React, { Component } from 'react';
-import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
 
-const items = [
-  {
-    src: 'https://i.imgur.com/yCjwSrZ.jpg',
-    altText: 'Our soccer team placed second in USA Disney Soccer Championship',
-    caption: 'Soccer Championship in Orlando, Florida'
-  },
-  {
-    src: 'https://i.imgur.com/jovIZXq.jpg',
-    altText: 'Joined us at the Light Kids Centers pre-New Year celebration and dance showcase',
-    caption: 'Public Advocate Candidate Tony Herbert supporting Light Kids Center'
-  },
-  {
-    src: 'https://i.imgur.com/a1EyhcL.jpg',
-    altText: 'Our dancers perform many dances such as Chechenzki',
-    caption: 'Dance Performance in Master Theatre'
-  }
-];
+// const items = [
+//   {
+//     src: 'https://i.imgur.com/yCjwSrZ.jpg',
+//     altText: 'Our soccer team placed second in USA Disney Soccer Championship',
+//     caption: 'Soccer Championship in Orlando, Florida'
+//   },
+//   {
+//     src: 'https://i.imgur.com/jovIZXq.jpg',
+//     altText: 'Joined us at the Light Kids Centers pre-New Year celebration and dance showcase',
+//     caption: 'Public Advocate Candidate Tony Herbert supporting Light Kids Center'
+//   },
+//   {
+//     src: 'https://i.imgur.com/a1EyhcL.jpg',
+//     altText: 'Our dancers perform many dances such as Chechenzki',
+//     caption: 'Dance Performance in Master Theatre'
+//   }
+// ];
 
 class ImageSlider extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeIndex: 0 };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
-  }
 
-  onExiting() {
-    this.animating = true;
+    this.state = {}
   }
-
-  onExited() {
-    this.animating = false;
-  }
-
-  next() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  previous() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
-  }
-
   render() {
-    const { activeIndex } = this.state;
-
-    const slides = items.map((item) => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.src}
-        >
-          <img src={item.src} alt={item.altText} />
-          <CarouselCaption captionText={item.altText} captionHeader={item.caption} />
-        </CarouselItem>
-      );
-    });
-
     return (
-      <Carousel
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-      >
-        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-        {slides}
-        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-      </Carousel>
-    );
+      <div className="slider">
+
+      </div>
+    )
   }
-} 
+};
 
 export default ImageSlider;

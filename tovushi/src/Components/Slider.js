ca3807c65f28e1611ecf21499a2ba95b04fb333d
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-14 17:35:12 -0500

    combined slider components

--- tovushi/src/Components/Slider.js
+++ tovushi/src/Components/Slider.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Slide from './slide';
+import LeftArrow from './LeftArrow';
+import RightArrow from './RightArrow';
 
-const Slider = () => {
-  return <div className="slider">
-  
-  </div>
+class Slider extends Compontent {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="slider">
+        <Slide />
+
+        <LeftArrow />
+        <RightArrow />
+      </div>
+    )
+  }
 }
 
 export default Slider;

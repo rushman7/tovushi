d8ccf69948a1ed2e2eea01aec9ce3c54db56925e
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-25 09:56:08 -0500

    removed old slider, working on new one

--- tovushi/src/Components/Home.js
+++ tovushi/src/Components/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
-import Slider from './Slider';
 
 class Home extends Component {
   render() {
@@ -9,7 +8,6 @@ class Home extends Component {
         <Nav />
         <div className="home_page">
           <h2 className="home_title">Welcome to Light Kids Center</h2>
-          <Slider />
         </div>
       </React.Fragment>
     )

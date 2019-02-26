8c5da665a2d2f7eb021199f07aeae3550a56c40e
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-26 16:03:37 -0500

    scrapping again, revisintg slider

--- tovushi/src/Components/Slider.js
+++ tovushi/src/Components/Slider.js
@@ -3,23 +3,15 @@ import React, { Component } from 'react';
 class Slider extends Component {
   render () {
     return (
-      <div className="slider-container">
+      <div className="slider-container middle">
         <div className="slides">
-          <input type="radio" className="radio-button" id="r1" />
-          <input type="radio" className="radio-button" id="r2" />
-          <input type="radio" className="radio-button" id="r3" />
-          <div className="slide">
-            <img src="https://i.imgur.com/yCjwSrZ.jpg" alt=""/>
+          <div className="slide s1">
             <img src="https://i.imgur.com/jovIZXq.jpg" alt=""/>
+            <img src="https://i.imgur.com/yCjwSrZ.jpg" alt=""/>
             <img src="https://i.imgur.com/a1EyhcL.jpg" alt=""/>
           </div>
-          <div className="navigation">
-            <label htmlFor="r1" class="label"></label>
-            <label htmlFor="r2" class="label"></label>
-            <label htmlFor="r3" class="label"></label>
-          </div>
         </div>
-      </div>  
+      </div>
     )
   }
 }

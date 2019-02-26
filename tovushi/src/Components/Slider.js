f8a0ac52b6d449db94ef5ee1bb7185f8b15b4996
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-26 16:52:40 -0500

    new image slider setup

--- tovushi/src/Components/Slider.js
+++ tovushi/src/Components/Slider.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import Card from './Card';
+import data from '../data/data';
 
 class Slider extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: data.images,
+      image: data.images[0]
+    }
+  }
+
+  nextSlide = () => {
+    const newIndex = this.state.image.index+1
+    this.setState({
+      image: data.images[newIndex]
+    })
+  }
+
+  prevSlide = () => {
+    const newIndex = this.state.image.index-1
+    this.setState({
+      image: data.images[newIndex]
+    })
+  }
+
   render () {
+    const {image} = this.state;
     return (
-      <div className="slider-container middle">
-        <div className="slides">
-          <div className="slide s1">
-            <img src="https://i.imgur.com/jovIZXq.jpg" alt=""/>
-            <img src="https://i.imgur.com/yCjwSrZ.jpg" alt=""/>
-            <img src="https://i.imgur.com/a1EyhcL.jpg" alt=""/>
-          </div>
-        </div>
+      <div className="slider-container">
+        <Card image={image} />        
+        <button 
+          onClick={() => this.nextSlide()}
+          disabled={image.index === data.images.length-1}> 
+          Next 
+        </button>
+        <button 
+          onClick={() => this.prevSlide()}
+          disabled={image.index === 0}> 
+          Prev
+        </button>
       </div>
     )
   }
---
+++ tovushi/src/data/data.js
@@ -0,0 +1,27 @@
+const data = {
+  "images":  [
+    {
+      "_id": "0s8div024712j02fu202u",
+      "index": 0,
+      "title": "Test 1",
+      "about": "About Test 1",
+      "picture": "https://i.imgur.com/yCjwSrZ.jpg" ,
+    },
+    {
+      "_id": "j1k014uv01nvl41u40o1",
+      "index": 1,
+      "title": "Test 2",
+      "about": "About Test 2",
+      "picture": "https://i.imgur.com/jovIZXq.jpg" ,
+    },
+    {
+      "_id": "dsv87sv092g20780g2g2",
+      "index": 2,
+      "title": "Test 3",
+      "about": "About Test 3",
+      "picture": "https://i.imgur.com/a1EyhcL.jpg" ,
+    }
+  ]
+}
+
+export default data;

d9e064978c4e0e2944a7d1c6ebd032a2458e79f1
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-28 13:52:31 -0500

    testing

--- tovushi/src/Components/Slider.js
+++ tovushi/src/Components/Slider.js
@@ -1,5 +1,51 @@
 import React, { Component } from 'react';
 
+let sliderImages = document.querySelectorAll('.slide')
+let arrowLeft = document.querySelector('#arrow-left')
+let arrowRight = document.querySelector('#arrow-right')
+let current = 0;
+
+function reset() {
+  for(let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = 'none';
+  }
+}
+
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = 'block';
+}
+
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = 'block';
+  current--;
+}
+
+if (arrowLeft) {
+  arrowLeft.addEventListener('click', function() {
+    if(current === 0) {
+      current = sliderImages.length;
+    }
+    slideLeft();
+  }) 
+}
+
+
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = 'block';
+  current++;
+}
+
+if(arrowRight) {
+  arrowRight.addEventListener('click', function() {
+    if(current === sliderImages.length - 1) {
+      current = -1;
+    }
+    slideRight();
+  })
+}
 
 class Slider extends Component {
   render () {
@@ -28,5 +74,7 @@ class Slider extends Component {
     )
   }
 }
+
+startSlide();
 
 export default Slider;

700e16a9c61a811fad0f9752e68112f57c8025ff
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-02-28 11:34:51 -0500

    working on new slider

--- tovushi/src/Components/Slider.js
+++ tovushi/src/Components/Slider.js
@@ -5,6 +5,25 @@ class Slider extends Component {
   render () {
     return (
       <div className="slider-container">
+        <div id ="arrow-left" className="arrow"></div>
+        <div className="slider">
+          <div className="slide slide1">
+            <div className="slide-content">
+              <span>Image One</span>
+            </div>
+          </div>
+          <div className="slide slide2">
+            <div className="slide-content">
+              <span>Image Two</span>
+            </div>
+          </div>
+          <div className="slide slide3">
+            <div className="slide-content">
+              <span>Image Three</span>
+            </div>
+          </div>
+        </div>
+        <div id ="arrow-right" className="arrow"></div>
       </div>
     )
   }
